950a1ba9556e51a499eaddb2840dd525cf0e073b
caitlinmag <caitlinmaguire14@outlook.com>
2025-10-19 14:50:34 +0100

    Working on Movies API and clean up code
    I moved the code for displaying a single movie API data into the movieService class. The link is added in to the program.cs file aswell. I also cleaned up the code and removed useless code comments.

--- SOA_CA1/obj/Debug/net8.0/SOA_CA1.AssemblyInfo.cs
+++ SOA_CA1/obj/Debug/net8.0/SOA_CA1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13fc1d121f806aa5d3955e526f5ef5020b8fefa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e8f9b7871fcb189c9c9a6ac956b1cdc7246d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
