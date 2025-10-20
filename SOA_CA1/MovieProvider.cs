87669d6a76aba2a3409c3f745df1fac97957c390
caitlinmag <caitlinmaguire14@outlook.com>
2025-10-20 19:12:26 +0100

    Working on second API
    I have started my feature for the second API, I am trying to show movie providers based on the id from the movie the user has input.

---
+++ SOA_CA1/MovieProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace SOA_CA1
+{
+    public class MovieProvider
+    {
+        public string title_id {  get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string ios_appstore_url { get; set; }
+        public string android_playstore_url { get; set; }
+        public List<string> regions { get; set; }
+
+        public MovieProvider() { }
+    }
+}
